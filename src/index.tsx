aec2605d2b9c152e6fad756673900446da5de10c
syuniki <s.tasaka@gmail.com>
2021-09-23 10:04:40 +0900

    コンポーネントの基本的な書き方

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App message='Hello, React!'/>,  // Appコンポーネントに message というデータ(プロップス)を渡す
+    document.getElementById('root')
+);
