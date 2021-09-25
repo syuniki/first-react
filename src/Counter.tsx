55aba482206b7394b66402b471d56c829ea1a646
syuniki <s.tasaka@gmail.com>
2021-09-25 10:13:34 +0900

    useRef, useEffectの基本

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const Counter: React.FC<{}> = () => {
     const initialValue: any = 0;
@@ -6,16 +6,23 @@ const Counter: React.FC<{}> = () => {
 
     const increment = () => {
         setValue((prevState) => prevState + 1);
-    }
+    };
     const decrement = () => {
         setValue((prevState) => prevState - 1);
-    }
+    };
+
+    const renderTimes = useRef<number>(0);
+    useEffect(() => {
+        renderTimes.current += 1;
+        console.log('render was done.');
+    });
 
     return (
         <div>
             <div>value : { value }</div>
             <button onClick={increment}>+1</button>
             <button onClick={decrement}>-1</button>
+            <div>This component was re-rendered { renderTimes.current } times!</div>
         </div>
     );
 };
