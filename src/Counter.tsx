aaea14870fb983e1d07450df2e49e746ee77b48d
syuniki <s.tasaka@gmail.com>
2021-09-25 10:50:09 +0900

    useRefの基本的な利用の方法<DOM>

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';   // インポート
 
 const Counter: React.FC<{}> = () => {
     const initialValue: any = 0;
@@ -12,10 +12,18 @@ const Counter: React.FC<{}> = () => {
     };
 
     const renderTimes = useRef<number>(0);
-    useEffect(() => {
+    useEffect(() => {  // useEffect に渡された関数はレンダーの結果が画面に反映された後に動作する
         renderTimes.current += 1;
         console.log('render was done.');
     });
+
+    const ref = useRef<HTMLInputElement>(null); // null! ← non null assertion operator (実際のデータにはnullはわたらないよいう明示的な表示)
+    const focusInput = () => {
+        // if(ref.current !== null) {
+        //     ref.current.focus();
+        // }
+        ref.current?.focus();  // ref.corrent が null の場合に処理をやめてくれる
+    }
 
     return (
         <div>
@@ -23,6 +31,8 @@ const Counter: React.FC<{}> = () => {
             <button onClick={increment}>+1</button>
             <button onClick={decrement}>-1</button>
             <div>This component was re-rendered { renderTimes.current } times!</div>
+            <input ref={ref} type="text" />
+            <button onClick={focusInput}>Click Me!</button>
         </div>
     );
 };
